4a4a46164e38e16fe16c7df927deb834f0cd9c76
Ivan Gabriel Guerra <ivanguerra09@gmail.com>
2022-05-26 03:59:20 -0300

    empezado a escribir el post sobre el componente switch

--- src/App.tsx
+++ src/App.tsx
@@ -5,48 +5,24 @@ import Switch from "./components/Switch";
 function App() {
   const [valueOne, setValueOne] = useState(false);
   const [valueTwo, setValueTwo] = useState(false);
-  const [valueThree, setValueThree] = useState(false);
 
   return (
     <div className="App">
-      <div>
-        <Switch
-          id="input-checkbox-1"
-          isOn={valueOne}
-          handleToggle={() => setValueOne(!valueOne)}
-          onBgColor="#039be5"
-          size="sm"
-        />
-        <p>
-          Size: <b>sm</b>
-        </p>
-      </div>
+      <Switch
+        id="input-checkbox-1"
+        isOn={valueOne}
+        handleToggle={() => setValueOne(!valueOne)}
+        onBgColor="#039be5"
+        size="sm"
+      />
 
-      <div>
-        <Switch
-          id="input-checkbox-2"
-          isOn={valueTwo}
-          handleToggle={() => setValueTwo(!valueTwo)}
-          onBgColor="#d81b60"
-          size="md"
-        />
-        <p>
-          Size: <b>md</b>
-        </p>
-      </div>
-
-      <div>
-        <Switch
-          id="input-checkbox-3"
-          isOn={valueThree}
-          handleToggle={() => setValueThree(!valueThree)}
-          onBgColor="#43a047"
-          size="lg"
-        />
-        <p>
-          Size: <b>lg</b>
-        </p>
-      </div>
+      <Switch
+        id="input-checkbox-2"
+        isOn={valueTwo}
+        handleToggle={() => setValueTwo(!valueTwo)}
+        onBgColor="#d81b60"
+        size="md"
+      />
     </div>
   );
 }
